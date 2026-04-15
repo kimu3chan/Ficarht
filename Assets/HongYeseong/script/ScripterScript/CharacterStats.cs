5ee36c9be81b229b564d0875c861fdbd96f38e6b
hongyeseong <eunpyohong44@gmail.com>
2026-04-15 17:16:30 +0900

    Update CharacterStats.cs

--- Assets/HongYeseong/script/ScripterScript/CharacterStats.cs
+++ Assets/HongYeseong/script/ScripterScript/CharacterStats.cs
@@ -5,11 +5,12 @@ public class CharacterStats : ScriptableObject
 {
     public string characterName;
 
-    [Header("Stats")]
-    public int health;      // 체력
-    public int stamina;     // 스테미너
-    public int power;    // 힘
-    public int defense;     // 방어력
-    public int intelligence;   // 지식
+    [Header("Stats")] public int health; // 체력
+    public int stamina; // 스테미너
+    public int power; // 힘
+    public int defense; // 방어력
+    public int intelligence; // 지식
     public float speed; // 속도
+
+    [Header("Player")] public GameObject player;
 }
