d2f6dcbde0d1bc6003f544b3281ea3250c2d1f34
kimu3chan <kimu3chan@gmail.com>
2026-04-15 17:16:18 +0900

    Revert "Merge pull request #2 from hee119/main"
    This reverts commit 4607656df90b584c57e7f0dc03d472611bc44b38, reversing
    changes made to 8954f2269406599865d750f81ec0bf1b440e5c9b.

--- Assets/HongYeseong/script/Charactor/CharaStat.cs
+++ Assets/HongYeseong/script/Charactor/CharaStat.cs
@@ -6,10 +6,9 @@ public class CharaStat : MonoBehaviour
 {
     public CharacterStats characterStats; //캐릭터 스탯
     Vector2 inputVector;
-    public Vector3 moveVector {get; private set;}
-    private Rigidbody CharctorRb;
+    Vector3 moveVector;
+    Rigidbody CharctorRb;
     float moveSpeed;
-    AnimManager animManager;
     public enum status // 캐릭터 현재 상태
     {
         Default,
@@ -23,21 +22,17 @@ public class CharaStat : MonoBehaviour
     { 
         CharctorRb = GetComponent<Rigidbody>();
         moveSpeed = characterStats.speed;
-        animManager = GetComponent<AnimManager>();
         Debug.Log(moveSpeed);
         Debug.Log(CharctorRb);
-        Debug.Log(animManager);
     }
-    void FixedUpdate() 
+    void Update() 
     {
         CharctorRb.linearVelocity = moveVector.normalized * moveSpeed;    
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        inputVector = context.ReadValue<Vector2>();
+    public void OnMove(InputValue value)
+    { 
+        inputVector = value.Get<Vector2>();
         moveVector = new Vector3(inputVector.x, 0f, inputVector.y);
-        animManager.MoveAnim(inputVector);
-        Debug.Log(moveVector);
     }
 }
