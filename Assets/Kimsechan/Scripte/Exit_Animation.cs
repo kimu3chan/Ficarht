65dc843dfb2ecdfa34d665796dd9dc7e840f03a5
kimu3chan <kimu3chan@gmail.com>
2026-05-07 16:50:56 +0900

    최근거 UI거의 다 만들어서 올린거임

--- Assets/Kimsechan/Scripte/Exit_Animation.cs
+++ Assets/Kimsechan/Scripte/Exit_Animation.cs
@@ -5,8 +5,13 @@ public class Exit_Animation : MonoBehaviour
 {
     [Header("설정")]
     [SerializeField] private Vector3 targetScale = Vector3.one;
+
     public float timeX = 0.5f;
     public float timeZ = 0.5f;
+
+    [Header("애니메이션 사이 간격")]
+    public float interval = 0.1f;
+
     public void OnEnable()
     {
         transform.localScale = Vector3.one * 0.001f;
@@ -18,20 +23,33 @@ public class Exit_Animation : MonoBehaviour
         transform.DOKill();
 
         Sequence seq = DOTween.Sequence();
-        seq.Append(transform.DOScaleZ(targetScale.z, timeZ).SetEase(Ease.InCubic))
-            .Append(transform.DOScaleX(targetScale.x, timeX).SetEase(Ease.OutCubic));
+
+        seq.Append(transform.DOScaleZ(targetScale.z, timeZ)
+                .SetEase(Ease.InCubic))
+
+            .AppendInterval(interval)
+
+            .Append(transform.DOScaleX(targetScale.x, timeX)
+                .SetEase(Ease.OutCubic));
     }
 
     public void CloseExitAnimation()
     {
         transform.DOKill();
+
         Sequence seq = DOTween.Sequence();
 
-        seq.Append(transform.DOScaleX(0.001f, timeX).SetEase(Ease.InCubic))
-            .Append(transform.DOScaleZ(0.001f, timeZ).SetEase(Ease.OutCubic));
+        seq.Append(transform.DOScaleX(0.001f, timeX)
+                .SetEase(Ease.InCubic))
+
+            .AppendInterval(interval)
+
+            .Append(transform.DOScaleZ(0.001f, timeZ)
+                .SetEase(Ease.OutCubic));
 
         seq.OnComplete(() => gameObject.SetActive(false));
     }
+
     public void Exit()
     {
 #if UNITY_EDITOR
